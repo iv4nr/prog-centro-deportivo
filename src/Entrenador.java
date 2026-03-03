33faee3dc96d2d85691f46b0fad734eb0c0b051b
Iván Rodríguez <ivanruiz@myuax.com>
2026-03-03 08:59:31 +0100

    feat(Entrenador): Atributos creados, constructor creado, metodos getter y metodo to string

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,3 +1,56 @@
 public class Entrenador {
 
+    private int  id;
+    private String nombre;
+    private String especialidad;
+    private int añosExperiencia;
+
+    /**
+     * Metodo constructor de Entrenador
+     * @param id id del entrenador
+     * @param nombre nombre del entrenador
+     * @param especialidad especialidad del entrenador
+     */
+    public Entrenador(int id, String nombre, String especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    /**
+     *Devuelve el id del entrenador
+     * @return id entrenador
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Devuelve el nombre del entrenador
+     * @return nombre del entrenador
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Devuelve la especialidad del entrenador
+     * @return especialidad
+     */
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Entrenador { ");
+        sb.append(String.format("id: %d,", id));
+        sb.append(String.format("nombre: %s,", nombre));
+        sb.append(String.format("especialidad: %s,", especialidad));
+        sb.append(" }");
+
+        return sb.toString();
+    }
+
 }
