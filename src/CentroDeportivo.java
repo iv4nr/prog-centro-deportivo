b796acc376af6b32b9cd5ded59b0db1cd3900c1c
Iván Rodríguez <ivanruiz@myuax.com>
2026-03-03 09:09:35 +0100

    feat(CentroDeportivo): metodo buscarEntrenador creado

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -8,6 +8,23 @@ public class CentroDeportivo {
         entrenadores = new Entrenador[NUM_MAX_ENTRENADORES];
     }
 
+    /**
+     * Busca un entrenador por su id
+     * @param id_entrenador id del entrenador recibido
+     * @return el entrenador o null
+     */
+    public Entrenador buscarEntrenador(int id_entrenador) {
+        boolean seguirBuscando = true;
+        Entrenador entrenadorEncontrado = null;
+
+        for (int i = 0; i < NUM_MAX_ENTRENADORES &&  seguirBuscando; i++) {
+            if (entrenadores[i] != null && entrenadores[i].getId() == id_entrenador) {
+                entrenadorEncontrado = entrenadores[i];
+                seguirBuscando = false;
+            }
+        }
+        return entrenadorEncontrado;
+    }
 
 
 }
