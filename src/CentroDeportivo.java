4cd102732f57bb9b2830334c225843c5f743c60d
Iván Rodríguez <ivanruiz@myuax.com>
2026-03-03 09:05:43 +0100

    feat(CentroDeportivo): Atributos creados y constructor creado

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -1,2 +1,13 @@
 public class CentroDeportivo {
+
+    private Entrenador[] entrenadores;
+    private final int NUM_MAX_ENTRENADORES;
+
+    public CentroDeportivo(int num_entrenadores) {
+        NUM_MAX_ENTRENADORES = num_entrenadores;
+        entrenadores = new Entrenador[NUM_MAX_ENTRENADORES];
+    }
+
+
+
 }
